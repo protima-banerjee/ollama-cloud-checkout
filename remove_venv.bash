59285158fb1cce1ce71e8b7441b8f541cb901a48
Protima <protima@Protimas-MacBook-Pro.local>
2026-04-26 13:19:12 -0400

    initial checkout

---
+++ remove_venv.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VENV_NAME=~/.virtualenvs/ollama-cloud-checkout
+
+# Remove the virtual environment
+rm -rf $VENV_NAME
+
+echo "Virtual environment '$VENV_NAME' has been removed."
---
+++ setup_venv.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VENV_NAME=~/.virtualenvs/ollama_cloud
+PYTHON=/opt/homebrew/bin/python3.14
+
+# Create the virtual environment
+$PYTHON -m venv $VENV_NAME
+
+# Activate the environment
+# Use 'source' to ensure the environment is activated in your current shell session
+source $VENV_NAME/bin/activate
+
+# 4. (Optional) Upgrade pip and install requirements if they exist
+if [ -f "requirements.txt" ]; then
+    pip install --upgrade pip
+    pip install -r requirements.txt
+    echo "Dependencies installed from requirements.txt."
+fi
+
+echo "Virtual environment '$VENV_NAME' is ready and activated."
